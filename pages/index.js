05a1117ed3e75e188bd9f7466277ed5199b38cbf
Kat <katsolberg@gmail.com>
2018-12-10 12:05:07 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = props => {
+  return (
+    <div>
+      Hey!
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = props => {
+  return (
+    <div>
+      Sell!
+    </div>
+  );
+};
+
+export default Sell;
